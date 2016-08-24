33b8f388ee4e31ea4fbd35ade6f1a612ce2e8762
Robert Ruiz <Robert.S.Ruiz@gmail.com>
2016-08-24 07:53:26 -0400

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,47 +9,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def logged_in?
-   !!current_user
- end
- helper_method :logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
 
- # Method to use in filter to ensure the user is logged in
- def authenticate!
-   unless logged_in?
-     redirect_to login_path
-   end
- end
+  # Method to use in filter to ensure the user is logged in
+  def authenticate!
+    redirect_to login_path unless logged_in?
+  end
 
- # Assign the current user
- def current_user=(user)
-   session[:user_id] = user.id
- end
-
+  # Assign the current user
+  def current_user=(user)
+    session[:user_id] = user.id
+  end
 end
-
-
-
-
-# # Returns the current use if logged in
-# def current_user
-#   # if session[:user_id]
-#   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-#   # end
-# end
-# helper_method :current_user
-#
-# # Returns a boolean representing if the user is logged in
-# def logged_in?
-#   !!current_user
-# end
-# helper_method :logged_in?
-#
-# # Method to use in a filter to ensure the user is logged in
-# def authorize!
-#   redirect_to login_path unless logged_in?
-# end
-#
-# # Assign the current user
-# def current_user=(user)
-#   session[:user_id] = user.id
-# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823112938) do
+ActiveRecord::Schema.define(version: 20160823181102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cat_terms_tables", force: :cascade do |t|
+    t.integer  "term_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_cat_terms_tables_on_category_id", using: :btree
+    t.index ["term_id"], name: "index_cat_terms_tables_on_term_id", using: :btree
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,10 +35,9 @@ ActiveRecord::Schema.define(version: 20160823112938) do
     t.string   "name"
     t.string   "definition"
     t.string   "author"
-    t.integer  "category_id"
     t.string   "web_link"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +50,6 @@ ActiveRecord::Schema.define(version: 20160823112938) do
     t.datetime "updated_at",   null: false
   end
 
+  add_foreign_key "cat_terms_tables", "categories"
+  add_foreign_key "cat_terms_tables", "terms"
 end
